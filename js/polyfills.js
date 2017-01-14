31d7c2e1070c01f364eb2a6f98a3850ad0bfcedd
paulliu87 <paul.pliu87@gmail.com>
2017-01-13 18:13:14 -0700

    Add circles to the page

--- js/polyfills.js
+++ js/polyfills.js
@@ -22,7 +22,6 @@ function classReg( className ) {
 }
 
 // classList support for class management
-// altho to be fair, the api sucks because it won't accept multiple classes at once
 var hasClass, addClass, removeClass;
 
 if ( 'classList' in document.documentElement ) {
